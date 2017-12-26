d77d34f138474f0bc2f895c3aea1d46091dd42d2
claudejin <mgjin@se.kaist.ac.kr>
2017-12-26 19:33:43 +0900

    Analyze goal achievements

--- src/main/java/simvasos/SIMVASoS.java
+++ src/main/java/simvasos/SIMVASoS.java
@@ -1,73 +1,85 @@
 package simvasos;
 
-import mci.Main;
-import simvasos.propcheck.PropertyChecker;
-import simvasos.sa.method.SPRT;
-import simvasos.scenario.robot.RobotScenario;
+import org.apache.commons.lang3.StringUtils;
+import simvasos.scenario.mciresponse.MCIResponseScenario;
 import simvasos.simulation.Simulator;
+import simvasos.simulation.analysis.PropertyValue;
 import simvasos.simulation.analysis.Snapshot;
 import simvasos.simulation.component.Scenario;
 import simvasos.simulation.component.World;
+import simvasos.simulation.util.Pair;
 
-import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Date;
+import java.util.Random;
 
 /**
  * Created by mgjin on 2017-06-12.
  */
 public class SIMVASoS {
     public static void main(String[] args) {
-        if (args.length > 0 && args[0].equals("old")) {
-            String[] passedArgs = Arrays.copyOfRange(args, 1, args.length);
-            try {
-                Main.experimentMain(passedArgs);
-            } catch (IOException e) {
-                System.out.println("Error: Old version is not runnable");
-                e.printStackTrace();
-            } finally {
-                System.exit(0);
-            }
-        }
+        SimpleDateFormat datetimeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-        Scenario scenario = new RobotScenario(3);
+        System.out.println("Session starting: " + datetimeFormat.format(new Date()));
+
+        Pair<Integer, Integer> mapSize = new Pair<Integer, Integer>(4, 4);
+        MCIResponseScenario.PatientDistribution distPatient = MCIResponseScenario.PatientDistribution.Uniform;
+        int numPatient = mapSize.getLeft() * mapSize.getRight();
+        int numFirefighter = 2;
+        int[] sosTypeOfFirefighter = {0, 0, 2, 0}; // virtual, collaborative, acknowledged, directed
+        int numStartingPoint = 2;
+        int neighborhoodRange = 1;
+        int numSampling = 1000;
+
+        int endTick = (numPatient + numFirefighter - 1) / numFirefighter;
+
+        System.out.println("Map size: " + mapSize.getLeft() + " x " + mapSize.getRight());
+        System.out.println("# Trapped people: " + numPatient + " (goals)");
+        System.out.println("# Firefighter: " + numFirefighter);
+        System.out.println("SoS Types: " + sosTypeOfFirefighter[0] + " virtual, " + sosTypeOfFirefighter[1] + " collaborative, " + sosTypeOfFirefighter[2] + " acknowledged, " + sosTypeOfFirefighter[3] + " directed");
+        System.out.println("# Starting points: " + numStartingPoint + " different random location(s)");
+        System.out.println("Neighborhood range: " + neighborhoodRange + " cell(s)");
+        System.out.println("# Samples: " + numSampling);
+        System.out.println("End simulation tick: " + endTick);
+
+        Scenario scenario = new MCIResponseScenario(mapSize, distPatient, numPatient, numFirefighter, sosTypeOfFirefighter, numStartingPoint, neighborhoodRange);
         World world = scenario.getWorld();
-        PropertyChecker checker = scenario.getChecker();
 
-        SPRT sprt = new SPRT();
+        int goalAchievement = 0;
+        int goalAchievementSum = 0;
+        int perfectAchcievementCnt = 0;
+        int[] goalAchievementDist = new int[mapSize.getLeft() * mapSize.getRight() + 1];
 
-        for (int i = 1; i < 100; i++) {
-            sprt.reset(0.05, 0.05, 0.01, 0.01 * i);
-
-            while (sprt.isSampleNeeded()) {
-                ArrayList<Snapshot> simulationLog = Simulator.execute(world, 11);
-                sprt.addSample(checker.isSatisfied(simulationLog));
-            }
-
-            System.out.println("Theta: " + (0.01 * i) + ", Sample Size: " + sprt.getSampleSize() + ", Decision: " + sprt.getDecision());
+        for (int i = 0; i < numSampling; i++) {
+            world.setSeed(new Random().nextInt());
+            ArrayList<Snapshot> trace = Simulator.execute(world, endTick);
+            ArrayList<PropertyValue> properties = trace.get(endTick).getProperties();
+            goalAchievement = (int) properties.get(properties.size() - 2).value;
+            goalAchievementDist[goalAchievement]++;
+            goalAchievementSum += goalAchievement;
+            if (goalAchievement >= (numFirefighter * endTick))
+                perfectAchcievementCnt++;
         }
 
-//        int endTick = 50;
-//        int nPatient = 100;
-//        int nFireFighter = 3;
-//        SoSType sostype = SoSType.Virtual;
-//
-//        Scenario scenario = new MCIResponseScenario(sostype, nPatient, nFireFighter, 0, 0);
-//        World world = scenario.getWorld();
-//        PropertyChecker checker = scenario.getChecker();
-//
-//        SPRT sprt = new SPRT();
-//
-//        for (int i = 1; i < 100; i++) {
-//            sprt.reset(0.05, 0.05, 0.01, 0.01 * i);
-//
-//            while (sprt.isSampleNeeded()) {
-//                world.setSeed(new Random().nextLong());
-//                ArrayList<Snapshot> simulationLog = Simulator.execute(world, endTick);
-//                sprt.addSample(checker.isSatisfied(simulationLog));
-//            }
-//
-//            System.out.println("Theta: " + (0.01 * i) + ", Sample Size: " + sprt.getSampleSize() + ", Decision: " + sprt.getDecision());
-//        }
+        printGoalAchievementDistribution(goalAchievementDist, numSampling);
+
+        System.out.println("Average: " + 1.0 * goalAchievementSum / numSampling);
+
+        System.out.println("\nSession complete: " + datetimeFormat.format(new Date()));
+    }
+
+    public static void printGoalAchievementDistribution(int[] goalAchievementDist, int numSampling) {
+        System.out.println(StringUtils.repeat('=', 80));
+        int max = 0;
+        for (int i = 0; i < goalAchievementDist.length; i++) {
+            System.out.println(String.format("%2d: ", i)
+                    + StringUtils.repeat(
+                            '*', goalAchievementDist[i] > 0 ?
+                            Math.max(goalAchievementDist[i] * 100 / numSampling, 1)
+                            : 0)
+                    + String.format(" %.2f%%", goalAchievementDist[i] * 100.0 / numSampling));
+        }
+        System.out.println(StringUtils.repeat('=', 80));
     }
 }
